5e118116e642928a27b189b53273c3790cbf00fa
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2014-09-10 06:57:23 -0400

    Modify Invoice class; Add isExpired method

--- src/com/mnishiguchi/java/Invoice.java
+++ src/com/mnishiguchi/java/Invoice.java
@@ -8,6 +8,7 @@ public class Invoice
 {
     private String invoiceNumber; 
     private Date purchaseDate;
+    private String amount;
     private Stack<PurchasedArticle> purchasedArticles;
 	
 	// constructor
@@ -15,6 +16,30 @@ public class Invoice
     {
     	this.invoiceNumber = invoiceNumber; 
     }
+    public Invoice(String invoiceNumber, Date purchaseDate, String amount, Stack<PurchasedArticle> purchasedArticles)
+    {
+    	this.invoiceNumber = invoiceNumber;
+    	this.purchaseDate = purchaseDate;
+    	this.amount = amount;
+    	this.purchasedArticles = purchasedArticles;
+    }
+    
+    /**  A static method to check if an Invoice object is expired.
+     *  @param purchaseDate			a Date object to represent a purchase date
+     *  @param millisec						a life span in milliseconds
+     *  @return	true if the Invoice object is expired; false otherwise
+     */
+    public static boolean isExpired(Date purchaseDate, long life_millisec)
+	{
+        // get purchaseDate in milliseconds		
+		long purchaseDate_millisec = purchaseDate.getTime();
+		
+		// get today's Date in milliseconds
+		Date t = new Date();
+		long today_millisec = t.getTime();
+		
+    	return (today_millisec - purchaseDate_millisec) < life_millisec;
+	}
     
     // accessor methods
     public String getInvoiceNumber()
