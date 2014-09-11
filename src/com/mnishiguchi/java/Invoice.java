81f40e75185281474885af5c6520ab79ca4f71eb
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2014-09-11 11:36:03 -0400

    add-Invoice.article array

--- src/com/mnishiguchi/java/Invoice.java
+++ src/com/mnishiguchi/java/Invoice.java
@@ -6,46 +6,50 @@ import java.util.Stack;
 /** Shows an invoice associated with a specified invoice number */
 public class Invoice
 {
-    // static constants
-	public static final long LIFE_SPAN = 31556952000L;	// one year
+	// static constants
+	public static final long LIFE_SPAN = 31556952000L;    // one year
+	public static String[] articles = {
+			"Trounsers","M-Suits", "Coats","Shirts","Jackets","Uniforms",
+			"Sheets","Dresses", "Skirts","L-Suits","Blouses","Sweaters"
+			};
 	
 	// instance variables
 	private String invoiceNumber; 
-    private Date purchaseDate;
-    private double amount;
-    private Stack<Article> purchasedArticles;
+	private Date purchaseDate;
+	private double amount;
+	private Stack<Article> purchasedArticles;
 	
 	// constructor
-    public Invoice(String invoiceNumber)
-    {
-    	this.invoiceNumber = invoiceNumber; 
-    }
-    public Invoice(String invoiceNumber, Date purchaseDate, double amount, Stack<Article> purchasedArticles)
-    {
-    	this.invoiceNumber = invoiceNumber;
-    	this.purchaseDate = purchaseDate;
-    	this.amount = amount;
-    	this.purchasedArticles = purchasedArticles;
-    }
-    
-	/**  A static method to check if an Invoice object is expired by referencing the constant Invoice.LIFE_SPAN.
-     *  @param purchaseDate			a Date object to represent a purchase date
-     *  @return	true if the Invoice object is expired; false otherwise
-     */
-    public static boolean isExpired(Date purchaseDate)
+	public Invoice(String invoiceNumber)
 	{
-        // get purchaseDate in milliseconds		
+		this.invoiceNumber = invoiceNumber; 
+	}
+	public Invoice(String invoiceNumber, Date purchaseDate, double amount, Stack<Article> purchasedArticles)
+	{
+		this.invoiceNumber = invoiceNumber;
+		this.purchaseDate = purchaseDate;
+		this.amount = amount;
+		this.purchasedArticles = purchasedArticles;
+	}
+
+	/**  A static method to check if an Invoice object is expired by referencing the constant Invoice.LIFE_SPAN.
+	*  @param purchaseDate			a Date object to represent a purchase date
+	*  @return	true if the Invoice object is expired; false otherwise
+	*/
+	public static boolean isExpired(Date purchaseDate)
+	{
+		// get purchaseDate in milliseconds		
 		long purchaseDate_millisec = purchaseDate.getTime();
 		
 		// get today's Date in milliseconds
 		Date t = new Date();
 		long today_millisec = t.getTime();
 		
-    	return (today_millisec - purchaseDate_millisec) < LIFE_SPAN;
+		return (today_millisec - purchaseDate_millisec) < LIFE_SPAN;
 	}
-    
-    // accessor methods
-    public String getInvoiceNumber()
+
+	// accessor methods
+	public String getInvoiceNumber()
 	{
 		return invoiceNumber;
 	}
@@ -69,7 +73,7 @@ public class Invoice
 	{
 		this.purchasedArticles = purchasedArticles;
 	}
-    public double getAmount()
+	public double getAmount()
 	{
 		return amount;
 	}
--- src/com/mnishiguchi/java/NewPurchaseFrame.java
+++ src/com/mnishiguchi/java/NewPurchaseFrame.java
@@ -10,11 +10,7 @@ import javax.swing.*;
 public class NewPurchaseFrame extends JFrame
 {
 	// instance variables
-	private String[] articles = {
-			"Item0","Item1", "Item2","Item3","Item4","Item5",
-			"Item6","Item7", "Item8","Item9","Item10","Item11"
-			};
-	ArrayList<JRadioButton>radioButtons = new ArrayList<JRadioButton>(articles.length);
+	ArrayList<JRadioButton>radioButtons = new ArrayList<JRadioButton>(Invoice.articles.length);
 	private JTextField priceField;
 	private JSpinner qtySpinner;
 	private JButton button1;
@@ -25,86 +21,88 @@ public class NewPurchaseFrame extends JFrame
 		// configuration of the frame
 		this.setSize(400, 250);
 		this.setResizable(false);
-	    this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	    this.setTitle("Add New Purchase");
-	    this.setLocationRelativeTo(null);  // put it at the center of the screen
-	    
-	    // mainPanel
-	    JPanel mainPanel = new JPanel( new GridBagLayout() );
-	    
-	    // ----------- create a panel with radioButtons --------------------------
-	    JPanel radioPanel = new JPanel();
-	    radioPanel.setLayout( new GridBagLayout() );
-	    radioPanel.setBorder( BorderFactory.createTitledBorder("Articles") );
-	    
-	    // create radio buttons
-	    ButtonGroup group = new ButtonGroup();
-	    JRadioButton rb = null;
-	    int index = 0;
-	    for (String name : articles)
-	    {
-	    	rb = new JRadioButton(name);
-	    	radioButtons.add(rb);
-	    	group.add( radioButtons.get(index) );
-	    	index += 1;	// increment index
-	    }
-	    // add radioButtons to radioPanel
-	    addGridItem(radioPanel, radioButtons.get(0), 0, 0, 1, 1, GridBagConstraints.CENTER);
-	    addGridItem(radioPanel, radioButtons.get(1), 1, 0, 1, 1, GridBagConstraints.CENTER);
-	    addGridItem(radioPanel, radioButtons.get(2), 2, 0, 1, 1, GridBagConstraints.CENTER);
-	    addGridItem(radioPanel, radioButtons.get(3), 3, 0, 1, 1, GridBagConstraints.CENTER);
-	    addGridItem(radioPanel, radioButtons.get(4), 0, 1, 1, 1, GridBagConstraints.CENTER);
-	    addGridItem(radioPanel, radioButtons.get(5), 1, 1, 1, 1, GridBagConstraints.CENTER);
-	    addGridItem(radioPanel, radioButtons.get(6), 2, 1, 1, 1, GridBagConstraints.CENTER);
-	    addGridItem(radioPanel, radioButtons.get(7), 3, 1, 1, 1, GridBagConstraints.CENTER);
-	    addGridItem(radioPanel, radioButtons.get(8), 0, 2, 1, 1, GridBagConstraints.CENTER);
-	    addGridItem(radioPanel, radioButtons.get(9), 1, 2, 1, 1, GridBagConstraints.CENTER);
-	    addGridItem(radioPanel, radioButtons.get(10), 2, 2, 1, 1, GridBagConstraints.CENTER);
-	    addGridItem(radioPanel, radioButtons.get(11), 3, 2, 1, 1, GridBagConstraints.CENTER);
-	    
-	    // add radioPanel  to the mainPanel
-	    addGridItem(mainPanel, radioPanel, 0, 0, 1, 1, GridBagConstraints.CENTER);
-	    
-	    // ----------- create a Box with price & qty fields -------------------
-	    Box box1 = Box.createHorizontalBox();	  
-	    // add a label to box1
-	    box1.add( new JLabel("Unit Price : $") );    
-	    // add a text field to box1
-	    priceField = new JTextField(8);
-	    box1.add(priceField);    
-	    // add a separator to box1
-	    box1.add( Box.createHorizontalStrut(20) );	// blank space
-	    // add a text field to box1
-	    box1.add( new JLabel("Qty : ") );
-	    // add a spinner to box1
-	    qtySpinner = new JSpinner( new SpinnerNumberModel(0,0,32,1) );
-	    box1.add(qtySpinner);
-	    // add box1 to the mainPanel
-	    addGridItem(mainPanel, box1, 0, 1, 1, 1, GridBagConstraints.CENTER);
-	    
-	 // ----------- create a Box with a submit button --------------------
-	    Box box2 = Box.createHorizontalBox();	  
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setTitle("Add New Purchase");
+		this.setLocationRelativeTo(null);  // put it at the center of the screen
+	
+		// mainPanel
+		JPanel mainPanel = new JPanel( new GridBagLayout() );
+		
+		// ----------- create a panel with radioButtons --------------------------
+		JPanel radioPanel = new JPanel();
+		radioPanel.setLayout( new GridBagLayout() );
+		radioPanel.setBorder( BorderFactory.createTitledBorder("Articles") );
+		
+		// create radio buttons
+		ButtonGroup group = new ButtonGroup();
+		JRadioButton rb = null;
+		int index = 0;
+		for (String name : Invoice.articles)
+		{
+		 	// create radio buttons; set the first one selected by default
+			rb = (index == 0) ? new JRadioButton(name, true) : new JRadioButton(name);
+			
+			radioButtons.add(rb);
+			group.add( radioButtons.get(index) );
+			index += 1;	// increment index
+		}
+		// add radioButtons to radioPanel
+		addGridItem(radioPanel, radioButtons.get(0), 0, 0, 1, 1, GridBagConstraints.WEST);
+		addGridItem(radioPanel, radioButtons.get(1), 1, 0, 1, 1, GridBagConstraints.WEST);
+		addGridItem(radioPanel, radioButtons.get(2), 2, 0, 1, 1, GridBagConstraints.WEST);
+		addGridItem(radioPanel, radioButtons.get(3), 3, 0, 1, 1, GridBagConstraints.WEST);
+		addGridItem(radioPanel, radioButtons.get(4), 0, 1, 1, 1, GridBagConstraints.WEST);
+		addGridItem(radioPanel, radioButtons.get(5), 1, 1, 1, 1, GridBagConstraints.WEST);
+		addGridItem(radioPanel, radioButtons.get(6), 2, 1, 1, 1, GridBagConstraints.WEST);
+		addGridItem(radioPanel, radioButtons.get(7), 3, 1, 1, 1, GridBagConstraints.WEST);
+		addGridItem(radioPanel, radioButtons.get(8), 0, 2, 1, 1, GridBagConstraints.WEST);
+		addGridItem(radioPanel, radioButtons.get(9), 1, 2, 1, 1, GridBagConstraints.WEST);
+		addGridItem(radioPanel, radioButtons.get(10), 2, 2, 1, 1, GridBagConstraints.WEST);
+		addGridItem(radioPanel, radioButtons.get(11), 3, 2, 1, 1, GridBagConstraints.WEST);
+		
+		// add radioPanel  to the mainPanel
+		addGridItem(mainPanel, radioPanel, 0, 0, 1, 1, GridBagConstraints.CENTER);
+
+		// ----------- create a Box with price & qty fields -------------------
+		Box box1 = Box.createHorizontalBox();	  
+		// add a label to box1
+		box1.add( new JLabel("Unit Price : $") );    
+		// add a text field to box1
+		priceField = new JTextField(8);
+		box1.add(priceField);    
+		// add a separator to box1
+		box1.add( Box.createHorizontalStrut(20) );	// blank space
+		// add a text field to box1
+		box1.add( new JLabel("Qty : ") );
+		// add a spinner to box1
+		qtySpinner = new JSpinner( new SpinnerNumberModel(0,0,32,1) );
+		box1.add(qtySpinner);
+		// add box1 to the mainPanel
+		addGridItem(mainPanel, box1, 0, 1, 1, 1, GridBagConstraints.CENTER);
+
+		// ----------- create a Box with a submit button --------------------
+		Box box2 = Box.createHorizontalBox();	  
 		// create a button
-	    button1 = new JButton("          Submit          ");
-	    OnButtonClickListener handle = new OnButtonClickListener();
-	    button1.addActionListener(handle);
-	    // add button1
-	    box2.add(button1);
-	    // add box2 to the mainPanel
-	    addGridItem(mainPanel, box2, 0, 2, 1, 1, GridBagConstraints.CENTER);
-	    
-	    this.add(mainPanel);
-	    this.setVisible(true);		// show this frame    
+		button1 = new JButton("          Submit          ");
+		OnButtonClickListener handle = new OnButtonClickListener();
+		button1.addActionListener(handle);
+		// add button1
+		box2.add(button1);
+		// add box2 to the mainPanel
+		addGridItem(mainPanel, box2, 0, 2, 1, 1, GridBagConstraints.CENTER);
+		
+		this.add(mainPanel);
+		this.setVisible(true);		// show this frame    
 	}
 	
 	/** Helper Method to add  a components to a specified GridBag cell
-	 * @param p			a panel with the GridBagLayout
-	 * @param c			a JComponent item to add
-	 * @param x			the x position of the component
-	 * @param y			the y position of the component
-	 * @param width	number of columns spanned by the component
-	 * @param height	number of rows spanned by the component
-	 * @param align		one of the constants defined by GridBagConstraints class, 
+	 * @param p    a panel with the GridBagLayout
+	 * @param c    a JComponent item to add
+	 * @param x    the x position of the component
+	 * @param y    the y position of the component
+	 * @param width    number of columns spanned by the component
+	 * @param height    number of rows spanned by the component
+	 * @param align    one of the constants defined by GridBagConstraints class, 
 	 * 								which indicate where to place the component if it doesn't fill the space.
 	 */
 	private void addGridItem(JPanel p, JComponent c, int x, int y, int width, int height, int align)
@@ -130,15 +128,15 @@ public class NewPurchaseFrame extends JFrame
 	{
 		// ingredients of an Article object
 		private String name;
-	    private double price;
-	    private int quantity;
+		private double price;
+		private int quantity;
 		
 		@Override
 		public void actionPerformed(ActionEvent event)
 		{
 			if (event.getSource() == button1)
 			{
-				// ------------------ validate price field --------------------			
+				// ------------------ validate price field --------------------
 				String amount = priceField.getText();	// get a price from user input
 			
 				if ( amount.equals("") )	// ensure text field is not empty
@@ -150,27 +148,27 @@ public class NewPurchaseFrame extends JFrame
 				else if ( StringChecker.isFloat(amount) == false )
 				{
 					JOptionPane.showMessageDialog( NewPurchaseFrame.this, 
-                            "Invalid Dollar Amount  (Example : 123.45)", 
-                            "Message", JOptionPane.INFORMATION_MESSAGE);
+							"Invalid Dollar Amount  (Example : 123.45)", 
+							"Message", JOptionPane.INFORMATION_MESSAGE);
 					return;	// quit this procedure right now
 				}
-				// ------------------ process data ----------------------------			
+				// ------------------ process data ----------------------------
 				try
 				{
 					// get selected article
-				    for (JRadioButton rb : radioButtons)
-				    {
-				    	if ( rb.isSelected() )
-				    	{
-				    		name = rb.getText();
-				    	}
-				    }
-				    
+					for (JRadioButton rb : radioButtons)
+					{
+						if ( rb.isSelected() )
+						{
+							name = rb.getText();
+						}
+					}
+					
 					// get price
 					price =Double.parseDouble( amount );
 					
 					// get quantity
-				    quantity = (int) qtySpinner.getValue();
+					quantity = (int) qtySpinner.getValue();
 				}
 				catch (NumberFormatException ex)
 				{
@@ -182,7 +180,7 @@ public class NewPurchaseFrame extends JFrame
 				
 				new NewInvoiceFrame();	// show a new invoice's draft
 				NewPurchaseFrame.this.dispose();    // close this frame	
-			}				
+			}
 		}
 	}
 }
